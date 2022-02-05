34deed6868fb48edd3d65d8cbe68625526287385
davidfuentes <david.fuentes.sge@mango.com>
2022-02-05 09:11:02 +0100

    - Adding extra endpoint for health check - Adjust the ports

---
+++ src/main/java/io/reflectoring/buckpal/account/adapter/in/web/HealthyController.java
@@ -0,0 +1,18 @@
+package io.reflectoring.buckpal.account.adapter.in.web;
+
+
+import io.reflectoring.buckpal.common.WebAdapter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@WebAdapter
+@RestController
+public class HealthyController {
+
+    @GetMapping(path = "/health")
+    ResponseEntity<String> healthCheck() {
+
+        return ResponseEntity.ok("All fine");
+    }
+}
