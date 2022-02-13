4465ca0eb553962fc286eac8941a38dc24f67274
davidfuentes <david.fuentes.sge@mango.com>
2022-02-13 19:04:21 +0100

    New greeting controller v1

---
+++ src/main/java/io/reflectoring/buckpal/account/adapter/in/web/GreetingController.java
@@ -0,0 +1,22 @@
+package io.reflectoring.buckpal.account.adapter.in.web;
+
+import io.reflectoring.buckpal.common.WebAdapter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@WebAdapter
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+class GreetingController {
+
+	@GetMapping(path = "/greeting")
+	ResponseEntity<String> greeting() {
+		 return new ResponseEntity<>("Hello v1",HttpStatus.OK);
+	}
+}
